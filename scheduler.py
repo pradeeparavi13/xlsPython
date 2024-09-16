d2b089d797a80cdc5fd503d7bcf196be71e9c487
pradeeparavi13 <48331586+pradeeparavi13@users.noreply.github.com>
2024-09-16 14:58:07 +0530

    Update scheduler.py
    updated py

--- scheduler.py
+++ scheduler.py
@@ -16,6 +16,17 @@ def modify_excel():
     else:
         print(f"File {output_file_path} does not exist.")
 
+    # Define the file path
+    output_file_path1 = './err_tco_out.xlsx'
+
+    # Check if the file exists
+    if os.path.exists(output_file_path1):
+        # Delete the file
+        os.remove(output_file_path1)
+        print(f"File {output_file_path} has been deleted.")
+    else:
+        print(f"File {output_file_path} does not exist.")
+
     # Step 1: Read the Excel file from the 'tco' sheet
     input_file_path = './err_tco.xlsx'
     df = pd.read_excel(input_file_path, sheet_name='Sheet2')
@@ -27,7 +38,7 @@ def modify_excel():
     df.drop(columns=[df.columns[9], df.columns[10]], inplace=True)
 
     # Step 4: Insert a new row in the first line
-    new_header = ['Name', 'AGID', 'AGID NAME', 'TABLE NAME', 'TABLE COUNT', 'APPLICATION NAME', 'APPLICATION DESCRIPTION', 'MNEMONIC', 'COST CODE']
+    new_header = ['Name', 'AGID', 'AGID NAME', 'TABLE NAME', 'TABLE COUNT', 'APPLICATION NAME', 'APPLICATION DESCRIPTION', 'MNEMONIC', 'COST CODE2']
     if len(new_header) == len(df.columns):
         df.loc[-1] = new_header  # adding a row
         df.index = df.index + 1  # shifting index
@@ -49,9 +60,9 @@ def modify_excel():
     # Step 8: Copy entire F column and paste special values in the same column
     df['NEW COUNT'] = df['NEW COUNT'].values
 
-    # Step 9: 'UNIQUE IDENTIFIER', 'COST CODE2'
+    # Step 9: 'UNIQUE IDENTIFIER', 'COST CODE'
     df.insert(10, 'UNIQUE IDENTIFIER', np.nan)
-    df.insert(11, 'COST CODE2', np.nan)
+    df.insert(11, 'COST CODE', np.nan)
 
     # num_columns = df.shape[1]
 
@@ -65,26 +76,87 @@ def modify_excel():
     # # Step 11: Copy column K and paste special values in the same column
     df['UNIQUE IDENTIFIER'] = df['UNIQUE IDENTIFIER'].values
     
-    # # Step 12: Add formula in column L (COST CODE2)
-    df['COST CODE2'] = df['COST CODE'].str[:8]
+    # # Step 12: Add formula in column L (COST CODE)
+    df['COST CODE'] = df['COST CODE2'].str[:8]
     
     # # Step 13: Copy column L and paste special values in the same column
-    df['COST CODE2'] = df['COST CODE2'].values
+    df['COST CODE'] = df['COST CODE'].values
     
     # # Step 14: Filter column J (COST CODE) for "BEA MMD"
-    df_filtered = df[df['COST CODE'] == 'BEA MMD']
+    df_filtered = df[df['COST CODE2'] == 'BEA MMED (Money Management Division)']
+
     
     # # Step 15: Add sum of the entire E column (TABLE COUNT) to its bottom
-    total_sum = df['TABLE COUNT'].sum()
-    df.loc[df.shape[0]] = ['', '', '', '', total_sum] + [''] * (df.shape[1] - 5)
+    total_sum = df_filtered['NEW COUNT'].sum()
+    # df.loc[df.shape[0]] = ['', '', '', '', total_sum] + [''] * (df.shape[1] - 5)
+
+    print("total_sum ==",total_sum)
+
+
+    df = df[~(df['COST CODE2'] == 'BEA MMED (Money Management Division)')]
+    count = len(df)
+    print("rows ===>>",len(df))
+
+
+    # Step 16: Read the second Excel file from the 'aca' sheet
+    input_file_path = './mmdcost.xlsx'
+    df1 = pd.read_excel(input_file_path, sheet_name='aca')
+
+    # Print column names to check for typos
+    print(df1.columns)
+
+
+    df1['acaPercentageallocation'] = (df1['acaPercentageallocation'] / 100) * total_sum / 2
+
+    df1.dropna(subset=['acaPercentageallocation'], inplace=True)
+
+    print(df1['acaPercentageallocation'])
+    print(df1['acaMu'])
+
+
+
+    # Create a new DataFrame with the values from df1['acaPercentageallocation']
+    # new_df = pd.DataFrame(df1['acaPercentageallocation'].values.reshape(-1, 1), columns=['NEW COUNT'])
+    new_df = pd.DataFrame({'NEW COUNT': df1['acaPercentageallocation'], 'COST CODE': df1['acaMu'], 'UNIQUE IDENTIFIER': df1['acaMu']})
+
+
+    # Insert the new DataFrame into df after the nth row
+    df = pd.concat([df[:count], new_df, df[count:]], ignore_index=True)
+
+    df.loc[count:, 'UNIQUE IDENTIFIER'] = "BEA MMD " + df.loc[count:, 'UNIQUE IDENTIFIER'].astype(str)
+    
+    df['COST CODE2'] = df['COST CODE2'].fillna('blank')
+    df['MNEMONIC'] = df['MNEMONIC'].fillna('MMD')
+
+    pivot_table = df.pivot_table(
+        index=['UNIQUE IDENTIFIER', 'COST CODE', 'COST CODE2', 'MNEMONIC'],
+    values='NEW COUNT',
+    aggfunc='sum',
+    fill_value=0
+    )
+
+    print(pivot_table)
+
+    pivot_table = pivot_table.reset_index()
+
+
+    pivot_table.to_excel('./pivot_table.xlsx', engine='openpyxl')
+
+    print("Updated values have been saved to", output_file_path)
+
+    # Print the DataFrame to verify the changes
+    # print(colVlaue)
     
     # Save the modified file to a different location
     output_file_path = './err_tco_out.xlsx'
     df.to_excel(output_file_path, index=False)
-    
-    # Step 15: Load the workbook to perform additional operations (if necessary)
+
+
+    # Step last: Load the workbook to perform additional operations (if necessary)
     wb = load_workbook(output_file_path)
     ws = wb.active
+    
+
     
     # If needed, you can perform more operations using openpyxl here
     
